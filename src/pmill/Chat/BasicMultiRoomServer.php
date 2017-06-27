bd1c473aba9007ab8dcd2b49fc9f11f91468186d
Oleg Skonnikov <anders2014@gmail.com>
2017-06-28 01:04:14 +0400

    Avatar
    Add avatar variable and functions

--- src/pmill/Chat/BasicMultiRoomServer.php
+++ src/pmill/Chat/BasicMultiRoomServer.php
@@ -9,17 +9,17 @@ class BasicMultiRoomServer extends AbstractMultiRoomServer
 
     protected function makeUserWelcomeMessage(ConnectedClientInterface $client, $timestamp)
     {
-        return vsprintf('Welcome %s!', array($client->getName()));
+        return vsprintf('Добро пожаловать, %s!', array($client->getName()));
     }
 
     protected function makeUserConnectedMessage(ConnectedClientInterface $client, $timestamp)
     {
-        return vsprintf('%s has connected', array($client->getName()));
+        return vsprintf('%s в сети', array($client->getName()));
     }
 
     protected function makeUserDisconnectedMessage(ConnectedClientInterface $client, $timestamp)
     {
-        return vsprintf('%s has left', array($client->getName()));
+        return vsprintf('%s не в сети', array($client->getName()));
     }
 
     protected function makeMessageReceivedMessage(ConnectedClientInterface $from, $message, $timestamp)
@@ -32,13 +32,13 @@ class BasicMultiRoomServer extends AbstractMultiRoomServer
         /** save messages to a database, etc... */
     }
 
-    protected function createClient(ConnectionInterface $conn, $name)
+    protected function createClient(ConnectionInterface $conn, $name, $avatar)
     {
         $client = new ConnectedClient;
         $client->setResourceId($conn->resourceId);
         $client->setConnection($conn);
         $client->setName($name);
-
+        $client->setAvatar($avatar);
         return $client;
     }
 
